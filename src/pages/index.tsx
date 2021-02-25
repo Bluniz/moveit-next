1fe914b2cd2389a1a2312b2fdb4a18f6d81ee484
Lucas Rosa <lucassantosrosa51@gmail.com>
2021-02-25 18:00:08 -0300

    Add aula 3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
+import Head from "next/head";
+
 import { CompletedChallenges } from "../components/CompletedChallengers";
 import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
-import styles from "../styles/pages/Home.module.css";
+import { ChallengeBox } from "../components/ChallengeBox";
 
-import Head from "next/head";
+import styles from "../styles/pages/Home.module.css";
 
 export default function Home() {
   return (
@@ -20,7 +22,9 @@ export default function Home() {
           <CompletedChallenges />
           <Countdown />
         </div>
-        <div></div>
+        <div>
+          <ChallengeBox />
+        </div>
       </section>
     </div>
   );
